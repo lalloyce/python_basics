c250fab6ab199794e06d672a5eda23ac49408126
Lawrence <lalloyce@gmail.com>
2024-10-24 13:52:05 +0300

    Initial commit

---
+++ operators.py
@@ -0,0 +1,9 @@
+maths = 70
+english = 60
+kiswahili = 55
+physics = 90
+
+mean_score = (maths + english + kiswahili + physics) / 4
+marks_remaining = 500 - (maths + english + kiswahili + physics)
+print(mean_score)
+print(marks_remaining)
---
+++ variables.py
@@ -0,0 +1,5 @@
+school = "eMobilis Mobile Technology Institute"
+maths = 89
+average = 89.0
+print(school)
+print(maths)
