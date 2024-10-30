cc85dcc8e908a77e550824b8e93cbd2c570a44f3
Lawrence <lalloyce@gmail.com>
2024-10-30 12:40:55 +0300

    grading assignment

--- class assignments/grading.py
+++ class assignments/grading.py
@@ -1,5 +1,31 @@
-def greeting ():
+def grading ():
     name = input("What is your name? ")
-    print(f"Hello {name}, welcome to the world of Python.")
 
-greeting()
+    print(f"Hello {name}, enter your marks for the following 5 subjects:")
+    maths = int(input("Maths: "))
+    english = int(input("English: "))
+    kiswahili = int(input("Kiswahili: "))
+    physics = int(input("Physics: "))
+    chemistry = int(input("Chemistry: "))
+
+    if maths <0 or maths > 100 or english <0 or english > 100 or kiswahili <0 or kiswahili > 100 or physics <0 or physics > 100 or chemistry <0 or chemistry > 100:
+        print("Invalid marks")
+        return
+
+    total = maths + english + kiswahili + physics + chemistry
+    average = total / 5
+
+    if average < 39 and average > 0:
+        grade = "D"
+    elif average < 59:
+        grade = "C"
+    elif average < 69:
+        grade = "B"
+    elif average < 1 and average > 100:
+        print("Invalid marks")
+    else:
+        grade = "A"
+
+    print(f"Hello {name}, your average is {average}, total is {total} and your grade is {grade}.")
+
+grading()
